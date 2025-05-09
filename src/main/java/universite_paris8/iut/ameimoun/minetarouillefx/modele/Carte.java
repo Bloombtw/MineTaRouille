6dc2df3c2245266f58f17ef465eec9088b9f331a
Anton Meimoun B2 <meimounanton@gmail.com>
2025-05-09 14:49:10 +0200

    Modification carte

--- src/main/java/universite_paris8/iut/ameimoun/minetarouillefx/modele/Carte.java
+++ src/main/java/universite_paris8/iut/ameimoun/minetarouillefx/modele/Carte.java
@@ -1,22 +1,41 @@
 package universite_paris8.iut.ameimoun.minetarouillefx.modele;
 
 public class Carte {
-    public static String[][] generationCarte() {
-        String[][] carte = new String[10][10];
+    public static final String[][] PROTOTYPE_CARTE = {
+            {"ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel"},
+            {"ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel"},
+            {"ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel"},
+            {"ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel"},
+            {"ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel"},
+            {"ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel", "ciel"},
+            {"terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre"},
+            {"terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre"},
+            {"terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre"},
+            {"terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre", "terre"}
+    };
+    public static void afficherCarte() {
+        for (String[] ligne : PROTOTYPE_CARTE) {
+            for (String cellule : ligne) {
+                System.out.print(cellule + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static String[][] genererCarte() {
+        String[][] carte = new String[5][5]; // Choisir la taille de la carte
+
+        //remplit avec deux motifs choisis
         for (int i = 0; i < carte.length; i++) {
             for (int j = 0; j < carte[i].length; j++) {
-                carte[i][j] = "-";
+                if (i < 3) {
+                    carte[i][j] = "."; // Ici le '.' veut dire ciel
+                } else {  // Terre dans les 2 dernières lignes
+                    carte[i][j] = "#"; // Changer les symboles ci besoin
+                }
             }
         }
         return carte;
     }
-
-    public static void afficherCarte(String[][] carte) {
-        for (int i = 0; i < carte.length; i++) {
-            System.out.println();
-            for (int j = 0; j < carte[i].length; j++) {
-                System.out.print(carte[i][j] + " ");
-            }
-        }
-    }
 }
+
