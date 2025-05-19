ef0a5de601a34b59a08f6885618bf272a90e5fca
Anton Meimoun B2 <meimounanton@gmail.com>
2025-05-19 14:13:18 +0200

    ajout léger de trucs dans constantes

--- src/main/java/universite_paris8/iut/ameimoun/minetarouillefx/utils/Constantes.java
+++ src/main/java/universite_paris8/iut/ameimoun/minetarouillefx/utils/Constantes.java
@@ -7,7 +7,7 @@ public class Constantes {
     public static final int NB_LIGNES = 35;
     public static final int NB_COLONNES = 62;
     public static final int NB_COUCHES = 3;
-    public static final int TAILLE_PERSO = TAILLE_TUILE;
+    public static final int TAILLE_PERSO = TAILLE_TUILE; // 30
     public static final int TAILLE_ITEM = 32;
     public static final int TAILLE_BOUTON = 200;
     public static final double GRAVITE = 0.2;
