c67030973d04d2f13742c880e065d077b2641146
sorana <ndiayesorana@gmail.com>
2025-06-16 12:20:43 +0200

    testJunit à compléter

---
+++ src/main/java/universite_paris8/iut/ameimoun/minetarouillefx/testJunit/TestInventaire.java
@@ -0,0 +1,4 @@
+package universite_paris8.iut.ameimoun.minetarouillefx.testJunit;
+
+public class TestInventaire {
+}
---
+++ src/main/java/universite_paris8/iut/ameimoun/minetarouillefx/testJunit/TestJoueur.java
@@ -0,0 +1,7 @@
+package universite_paris8.iut.ameimoun.minetarouillefx.testJunit;
+
+public class TestJoueur {
+
+    //test deplacement
+    //saut
+}
