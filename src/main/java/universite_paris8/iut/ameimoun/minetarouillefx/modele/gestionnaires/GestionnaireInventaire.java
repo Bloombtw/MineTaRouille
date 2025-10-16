1427a8015c2522ad3a18d67562648dd9bdce011c
sorana <ndiayesorana@gmail.com>
2025-10-16 16:18:25 +0200

    par défaut avant de merge

--- src/main/java/universite_paris8/iut/ameimoun/minetarouillefx/modele/gestionnaires/GestionnaireInventaire.java
+++ src/main/java/universite_paris8/iut/ameimoun/minetarouillefx/modele/gestionnaires/GestionnaireInventaire.java
@@ -36,7 +36,7 @@ public class GestionnaireInventaire {
         inventaire.ajouterItem(new Item(Bloc.PIERRE, 64));
 
         AnchorPane.setTopAnchor(vueInventaire, 10.0);
-        AnchorPane.setRightAnchor(vueInventaire, 100.0);
+        AnchorPane.setRightAnchor(vueInventaire, 10.0);
         joueurVue.mettreAJourObjetTenu(inventaire.getItem(inventaire.getSelectedIndex()));
         inventaire.selectedIndexProperty().addListener((obs, oldVal, newVal) -> {
             joueurVue.mettreAJourObjetTenu(inventaire.getItem(newVal.intValue()));
